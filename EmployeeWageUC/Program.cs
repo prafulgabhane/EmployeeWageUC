2d491ff8afadf968b7c994ed224d9f8d0033596d
prafulgabhane <prafulgabhane786@gmail.com>
2022-09-14 02:30:13 +0530

    Added All Files

---
+++ EmployeeWageUC/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageUC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int empPresent = 1;
+
+            // generating random value
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            Console.WriteLine("Random Value:" + empCheck);
+
+            // condition check if else
+            // testing comment line
+
+
+            if (empPresent == empCheck)
+                Console.WriteLine("Employee is Present");
+
+            else
+                Console.WriteLine("Employee is Absent");
+            Console.ReadLine();
+        }
+    }
+}
