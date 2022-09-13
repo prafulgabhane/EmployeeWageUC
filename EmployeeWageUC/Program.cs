44938206fc0b689c2ade60d50fcf8da64c5bc39b
prafulgabhane <prafulgabhane786@gmail.com>
2022-09-14 03:34:00 +0530

    Adding Employee Attendance program

--- EmployeeWageUC/Program.cs
+++ EmployeeWageUC/Program.cs
@@ -10,32 +10,23 @@ namespace EmployeeWageUC
     {
         static void Main(string[] args)
         {
-            //constant declaration
-            int IS_FULL_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
-            
-            //variable declaration
-            int empHrs = 0;
-            int empWage = 0;
+            int empPresent = 1;
+
+            // generating random value
 
             Random random = new Random();
             int empCheck = random.Next(0, 2);
+            Console.WriteLine("Random Value:" + empCheck);
 
-            if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
+            // condition check if else
+            // testing comment line
+
+            if (empPresent == empCheck)
+                Console.WriteLine("Employee is Present");
 
             else
-            {
-                empHrs = 0;
-            }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            //formula for understanding
-            Console.WriteLine("Emp Wage: "+empWage);
-            Console.WriteLine("empHour: [{1}] X empRatePerHour [{2}] = empWage: [{0}]", empWage, empHrs, EMP_RATE_PER_HOUR);
-
-            Console.ReadLine();
+                Console.WriteLine("Employee is Absent");
+                Console.ReadLine();
         }
     }
 }
