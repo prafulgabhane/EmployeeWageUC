ffbefd54fe0cf114a78ef3e6fe9b4917e357fc57
prafulgabhane <prafulgabhane786@gmail.com>
2022-09-15 15:08:25 +0530

    Adding UC 7 to main

--- EmployeeWageUC/Program.cs
+++ EmployeeWageUC/Program.cs
@@ -14,17 +14,17 @@ namespace EmployeeWageUC
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 2;
         public const int MAX_HRS_IN_MONTH = 10;
-        static void Main(string[] args)
+        public static int computeEmpWage()
         {
             //variable declaration
             int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
-            while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
                 totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
-                
+
                 switch (empCheck)
                 {
                     case IS_PART_TIME:
@@ -41,11 +41,17 @@ namespace EmployeeWageUC
 
                 }
                 totalEmpHrs += empHrs;
-                Console.WriteLine("Day " + totalWorkingDays + " Emp Hours: "+ empHrs);
+                Console.WriteLine("Day " + totalWorkingDays + " Emp Hours: " + empHrs);
             }
             int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total: Employee Wage: " + totalEmpWage);
+            Console.WriteLine("Total: Employee Wage :" + totalEmpWage);
             Console.ReadLine();
+            return totalEmpWage;
+
+        }
+        static void Main(string[] args)
+        {
+            computeEmpWage();
         }
     }
 }
